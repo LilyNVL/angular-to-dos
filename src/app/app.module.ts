76d1ed544ff48e1b3440db3442e9d4cf31c99711
Alisée Nollevalle <l.nollevalle@gmail.com>
2023-01-11 08:56:52 +0100

    add-todo & list-todo created

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { AddTodoComponent } from './add-todo/add-todo.component';
+import { ListTodoComponent } from './list-todo/list-todo.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    AddTodoComponent
+    AddTodoComponent,
+    ListTodoComponent
   ],
   imports: [
     BrowserModule,
