e296a8f4e68f4eb5b419aca7c27f46d116f5bd80
Alisée Nollevalle <l.nollevalle@gmail.com>
2023-01-17 08:45:48 +0100

    ///

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,6 +4,8 @@ import { AppComponent } from './app.component';
 import { ListTodoComponent } from './list-todo/list-todo.component';
 import { TodoInputComponent } from './todo-input/todo-input.component';
 
+/// J'ai préféré te rendre une version fonctionnelle donc le path ne marche pas, ou pas à grand chose ;)
+
 const routes: Routes = [
   {path:'add-todo', component: TodoInputComponent},
 ];
